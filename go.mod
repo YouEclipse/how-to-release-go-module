f210872077c6bbeae7fef972e5b7abbdcb82e05a
yoyo <chuiyouwu@gmail.com>
2020-02-20 20:35:44 +0800

    breaking changes

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/YouEclipse/how-to-release-go-module
+module github.com/YouEclipse/how-to-release-go-module/v2
 
 go 1.13
--- hello.go
+++ hello.go
@@ -3,11 +3,13 @@ package pkg
 import "fmt"
 
 // Hello says hello.
-func Hello() {
+func Hello() error {
 	fmt.Println("Hello go mod!")
+	return nil
 }
 
 // Bye says bye.
-func Bye() {
+func Bye() error {
 	fmt.Println("Bye go mod!")
+	return nil
 }
